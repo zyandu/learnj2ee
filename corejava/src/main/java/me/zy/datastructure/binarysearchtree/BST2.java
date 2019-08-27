e6441afb6019551082c7219a6e325739dcbca8f5
zhaoying <69968013@qq.com>
2019-08-28 01:51:31 +0800

    Update BST2.java
    二分查找树：前序、中序、后序遍历演示

--- corejava/src/main/java/me/zy/datastructure/binarysearchtree/BST2.java
+++ corejava/src/main/java/me/zy/datastructure/binarysearchtree/BST2.java
@@ -1,5 +1,8 @@
 package me.zy.datastructure.binarysearchtree;
 
+import java.util.Queue;
+import java.util.LinkedList;
+
 /**
  * 二叉搜索树
  * 每个元素必须具有可比较性
@@ -34,7 +37,7 @@ public class BST2<E extends Comparable<E>> {
 
     //插入一个元素
     public void add (E e){
-        add(root,e);
+        root = add(root,e);
     }
 
     // 向以node为根的二分搜索树中插入元素e，递归算法
@@ -80,12 +83,41 @@ public class BST2<E extends Comparable<E>> {
 
     //前序遍历以node为根节点的二分搜索树，递归算法
     private void preOrder(Node node){
-        if(node != null)
+        if(node == null)
             return;
 
         System.out.println(node.e);
         preOrder(node.left);
         preOrder(node.right);
+    }
+
+    //中序遍历
+    public void inOrder(){
+        inOrder(root);
+    }
+
+    // 中序遍历以node为根的二分搜索树, 递归算法
+    private void inOrder(Node node){
+        if(node == null)
+            return;
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    //后序遍历以node为根的二分搜索树, 递归算法
+    public void postOrder(){
+        postOrder(root);
+    }
+
+    private void postOrder(Node node){
+        if(node == null)
+            return;
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
     }
 
     @Override
@@ -108,16 +140,46 @@ public class BST2<E extends Comparable<E>> {
 
     private String generateDepthString(int depth){
         StringBuilder res = new StringBuilder();
-        for(int i = 0 ; i < depth ; i ++)
+        for(int i = 0 ; i < depth ; i ++) {
             res.append("--");
+        }
+
         return res.toString();
     }
 
+    /**
+     * 前序、中序、后序遍历都是深度遍历
+     * 下面演示广度遍历：同层级的在一起
+     */
+    public void levelOrder(){
+        if(root == null){
+            return;
+        }
+
+        Queue<Node> q = new LinkedList<Node>();
+        q.add(root);
+        while(!q.isEmpty()){
+            Node cur = q.remove();
+            System.out.println(cur.e);
+
+            if(cur.left != null){
+                q.add(cur.left);
+            }
+            if(cur.right != null){
+                q.add(cur.right);
+            }
+        }
+
+
+    }
+
     public static void main(String[] args) {
-        BST2<Integer> bst = new BST2<>();
+        BST2<Integer> bst2 = new BST2<>();
         int[] nums = {5, 3, 6, 8, 4, 2};
-        for(int num: nums)
-            bst.add(num);
+        for(int num: nums){
+            bst2.add(num);
+        }
+        //System.out.println(bst2);
 
         /////////////////
         //      5      //
@@ -126,9 +188,22 @@ public class BST2<E extends Comparable<E>> {
         //  / \    \   //
         // 2  4     8  //
         /////////////////
-        bst.preOrder();
-        System.out.println();
+        //bst2.preOrder(); //532468
+        //System.out.println();
+        //System.out.println(bst2);
+        //
+        //bst2.inOrder(); //234568
+        //System.out.println();
+        //System.out.println(bst2);
+        //
+        //bst2.postOrder(); //243865
+        //System.out.println();
+        //System.out.println(bst2);
 
-        System.out.println(bst);
+        bst2.levelOrder(); //536248
+        System.out.println();
+        System.out.println(bst2);
+
+
     }
 }
